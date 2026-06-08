8fa0423125baff68b57b87f77a8c232f6d532ac0
tonbonami <tonbonami@gmail.com>
2026-06-09 01:18:58 +0900

    임시: 손글씨 폰트 비교 페이지 /praise/fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&family=PT+Sans:wght@400;700&family=Gaegu:wght@400;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&family=PT+Sans:wght@400;700&family=Gaegu:wght@400;700&family=Hi+Melody&family=Nanum+Pen+Script&family=Cute+Font&family=Single+Day&family=Dongle:wght@400;700&family=East+Sea+Dokdo&family=Nanum+Brush+Script&display=swap" rel="stylesheet" />
       </head>
       <body className={cn("font-body antialiased")}>
         {children}
